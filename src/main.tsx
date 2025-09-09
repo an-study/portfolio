e8dd45176bae64e9456ca938a6d8ca4ed49a2dac
a.n <ao.nishigaki@gmail.com>
2025-09-09 19:20:37 +0900

    main.tsx を更新

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+
+// 👇 これを必ず入れる（Tailwindを含む全CSSがここで入ります）
+import "./index.css";
+
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
