de65aa260b7fc93d49f1ead763ec97f0bfa3e9d4
a.n <ao.nishigaki@gmail.com>
2025-09-09 19:16:57 +0900

    main.tsx を更新

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,11 @@
 // src/main.tsx
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <App />
+  </BrowserRouter>
+);
